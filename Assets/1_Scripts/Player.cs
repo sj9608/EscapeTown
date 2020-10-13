30805b11213eb003773710476276326d8e4a4c62
HeoJongHyun <ghkfls126@naver.com>
2020-10-13 12:44:37 +0900

    카메라 플레이어 시점

--- Assets/1_Scripts/Player.cs
+++ Assets/1_Scripts/Player.cs
@@ -128,6 +128,10 @@ public class Player : Human
         {
             OpenNote();
         }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ChangeWeapon();
+        }
         if (Input.GetKey(KeyCode.LeftControl))
         {
             isCrouch = true;
@@ -191,21 +195,21 @@ public class Player : Human
         //}
         // 플레이어 이동 방향에 대한 벡터
         Vector3 direction = new Vector3(inputX, 0f, inputZ).normalized;
+        // 플레이어가 움직이는 방향에 대한 회전을 하기 위해서
+        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camTransform.eulerAngles.y; // (x, z)벡터에 대한 각도 * 라디안 단위로 변환 + ( 카메라의 회전에 캐릭터가 대응하게 하기 위한 값)
+
+        // 캐릭터의 회전을 부드럽게 만들기 위한 처리 과정
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+        // 실제 회전 진행
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        // 카메라가 바라보는 방향에 대응하게 하기위한 벡터 (실제 움직임 벡터)
+        Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 
         // 움직임 검사
         if (direction.magnitude >= 0.1f) // 움직임이 있다면 (어느방향으로든 방향벡터값이)
         {
-            // 플레이어가 움직이는 방향에 대한 회전을 하기 위해서
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camTransform.eulerAngles.y; // (x, z)벡터에 대한 각도 * 라디안 단위로 변환 + ( 카메라의 회전에 캐릭터가 대응하게 하기 위한 값)
-
-            // 캐릭터의 회전을 부드럽게 만들기 위한 처리 과정
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-
-            // 실제 회전 진행
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            // 카메라가 바라보는 방향에 대응하게 하기위한 벡터 (실제 움직임 벡터)
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            
             if (isCrouch)
             {
                 moveSpeed = 1.5f;
@@ -225,7 +229,7 @@ public class Player : Human
                     moveSpeed = (moveSpeed > 5.0f) ? 5.0f : (moveSpeed + .1f);
                 }
             }
-            controller.Move(moveDir * moveSpeed * Time.deltaTime); // 해당 벡터의 방향으로 speed 수치만큼 frame간격마다 이동 (카메라가 바라보는 방향으로 움직이게 하기위해서 direction --> moveDir 교체)
+            controller.Move(transform.forward * moveSpeed * Time.deltaTime); // 해당 벡터의 방향으로 speed 수치만큼 frame간격마다 이동 (카메라가 바라보는 방향으로 움직이게 하기위해서 direction --> moveDir 교체)
         }
         else
         {
@@ -258,6 +262,10 @@ public class Player : Human
             Debug.Log("대화 수첩 닫힘");
         }
         isNoteOpen = !isNoteOpen;
+    }
+    void ChangeWeapon()
+    {
+        Debug.Log("무기 바뀜");
     }
     // 애니메이터의 IK 갱신
     private void OnAnimatorIK(int layerIndex)
