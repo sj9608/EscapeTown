2bf95e14794c745ef15f35c98b5728d6034e1c7c
LeeHann <0104ihn@naver.com>
2020-10-14 21:28:22 +0900

    ChatController_xml

--- Assets/1_Scripts/ChatController.cs
+++ Assets/1_Scripts/ChatController.cs
@@ -8,17 +8,20 @@ public class ChatController : MonoBehaviour
 {
     public Text chatText;
     public Text chatCharacter;
+    public int SceneNumber;
+    string loadFile;
 
     XmlNodeList nodes;
     void Start()
     {
+        loadFile = string.Format("Chat_{0}", SceneNumber);
         LoadXml();
         StartCoroutine(PrintText());
     }
 
     void LoadXml()
     {
-        TextAsset textAsset = (TextAsset)Resources.Load("Chat");
+        TextAsset textAsset = (TextAsset)Resources.Load(loadFile);
         Debug.Log(textAsset);
         XmlDocument xmlDoc = new XmlDocument();
         xmlDoc.LoadXml(textAsset.text);
@@ -28,8 +31,6 @@ public class ChatController : MonoBehaviour
         foreach (XmlNode node in nodes)
         {
             Debug.Log("Name :: " + node.SelectSingleNode("code").InnerText);
-            Debug.Log("Level :: " + node.SelectSingleNode("situation").InnerText);
-            Debug.Log("Exp :: " + node.SelectSingleNode("sentence").InnerText);
         }
     }
 
@@ -49,11 +50,13 @@ public class ChatController : MonoBehaviour
     IEnumerator PrintText()
     {
         string sentence = "";
-        
+        string speaker = "";
+
         foreach(XmlNode node in nodes)
         {
             sentence = node.SelectSingleNode("sentence").InnerText;
-            yield return StartCoroutine(NormalChat("당신", sentence));
+            speaker = node.SelectSingleNode("speaker").InnerText;
+            yield return StartCoroutine(NormalChat(speaker, sentence));
             yield return new WaitForSeconds(3); 
         }
     }
