69aea4c7d6fe64695248acbc2083943b08d454b3
LSJ <sneak7581@wku.ac.kr>
2020-10-20 10:19:56 +0900

    Aiming, Locomotion, Camera

---
+++ Assets/1_Scripts/CharacterAiming.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CharacterAiming : MonoBehaviour
+{
+    public float turnSpeed = 15f;
+    Camera mainCamera; // 카메라의 y축 회전에 대한 캐릭터 대응을 하기위한 카메라값 가져올 용도로 선언
+    public Transform cameraLookAt; // vCam 쳐다보는곳 
+    public Cinemachine.AxisState xAxis; // 시네머신 FreeLook 카메라에 있는 요소 가져오기 --> 에디터 상에서 수치 조절해줄 것.
+    public Cinemachine.AxisState yAxis;
+    
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    
+    private void FixedUpdate() 
+    {
+        float yawCamera = mainCamera.transform.rotation.eulerAngles.y; // 카메라의 y축 각도
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), turnSpeed * Time.fixedDeltaTime); // 캐릭터가 카메라의 회전에 대응하여 회전 하는 것
+
+        cameraLookAt.eulerAngles = new Vector3(yAxis.Value, xAxis.Value, 0);
+
+        xAxis.Update(Time.fixedDeltaTime);
+        yAxis.Update(Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/1_Scripts/CharacterLocomotion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterLocomotion : MonoBehaviour
+{
+    Animator animator;
+    Vector2 input;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical"); 
+
+        animator.SetFloat("InputX", input.x);
+        animator.SetFloat("InputY", input.y);
+    }
+}
