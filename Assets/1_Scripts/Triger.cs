5f912932ee1455c1db37453747fe54badc11d953
mahojiji <uk9931@naver.com>
2020-10-27 17:56:45 +0900

    stage06 함정트리거 업데이트

---
+++ Assets/1_Scripts/Triger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triger : MonoBehaviour
+{
+    public GameObject wall;
+    Vector3 target = new Vector3(0, -10, 0);
+    private Collider trap;
+    
+    void Start()
+    {
+        trap = GetComponent<Collider>();
+    }
+
+    IEnumerator OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {   
+            Debug.Log("Working");
+            wall.transform.position += target;
+        }
+        yield return null;
+    }
+}
