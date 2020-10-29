188e73893f39d90e540be8fdd148ad6884a1be53
HeoJongHyun <ghkfls126@naver.com>
2020-10-29 15:01:16 +0900

    아이템습득시 로그창 나오게 함

--- Assets/1_Scripts/UI/UILogPopUpWindow.cs
+++ Assets/1_Scripts/UI/UILogPopUpWindow.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 // ************* 아이템 획득 시 뜨는 팝업창에 대한 코드 ****************
 public class UILogPopUpWindow : MonoBehaviour
 {
+    GameInformation GI;
     public GameObject potion_LogWindow;
     public GameObject magazine_LogWindow;
     RectTransform rectTransform_1;
@@ -16,6 +17,17 @@ public class UILogPopUpWindow : MonoBehaviour
     float time = 0f;
     bool isShow = false;   // 현재 로그창이 떠있는지 안떠있는지 여부
 
+    private void Awake() {
+        GI = GameInformation.Instance;
+    }
+    private void OnEnable() {
+        GI.UpdateRemainAmmoAction += GetMagazineLog;
+        GI.UpdateGetPotionAction += GetPotionLog;
+    }
+    private void OnDisable() {
+        GI.UpdateRemainAmmoAction -= GetMagazineLog;
+        GI.UpdateGetPotionAction -= GetPotionLog;
+    }
     void Start()
     {
         rectTransform_1 = potion_LogWindow.GetComponent<RectTransform>();
@@ -23,54 +35,48 @@ public class UILogPopUpWindow : MonoBehaviour
         potion_LogWindow.SetActive(false);
         magazine_LogWindow.SetActive(false);
     }
-
-    void Update()
-    {
-
-        StartCoroutine(IELogPopUp());
-
-
+    private void GetMagazineLog(){
+        StartCoroutine(IELogMagazinePopUp());
     }
-    IEnumerator IELogPopUp()
+    private void GetPotionLog(){
+        StartCoroutine(IELogPotionPopUp());
+    }
+    IEnumerator IELogMagazinePopUp()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha3) && isShow == false)
+        magazine_LogWindow.SetActive(true);
+        isShow = true;
+
+        for (int i = 0; i < 10; ++i)
         {
-            potion_LogWindow.SetActive(true);
-            isShow = true;
+            float a = Vector3.Distance(rectTransform_2.position, target) / 10;
+            rectTransform_2.localPosition += new Vector3(-a, 0f, 0f); // 현 위치, 목표지점, 참조속도, 속도
 
-            for (int i = 0; i < 10; ++i)
-            {
-                float a = Vector3.Distance(rectTransform_1.position, target) / 10;
-                rectTransform_1.localPosition += new Vector3(-a, 0f, 0f); // 현 위치, 목표지점, 참조속도, 속도
-                Debug.Log(a);                                                                                     // ref : 참조접근 -> 실시간으로 바뀌는 값 적용 가능
-
-                yield return null;
-;
-            }
-                yield return new WaitForSeconds(0.5f);
-                potion_LogWindow.SetActive(false);
-                isShow = false;
-                rectTransform_1.localPosition = new Vector3(1090, -303, 0);
+            yield return null;
         }
-        
-        
 
-        if (Input.GetKeyDown(KeyCode.Alpha4) && isShow == false)
+        yield return new WaitForSeconds(0.5f);
+
+        magazine_LogWindow.SetActive(false);
+        isShow = false;
+        rectTransform_2.localPosition = new Vector3(1090, -303, 0);
+    }
+    IEnumerator IELogPotionPopUp()
+    {
+        potion_LogWindow.SetActive(true);
+        isShow = true;
+
+        for (int i = 0; i < 10; ++i)
         {
-            magazine_LogWindow.SetActive(true);
-            isShow = true;
-            for (int i = 0; i < 10; ++i)
-            {
-                float a = Vector3.Distance(rectTransform_2.position, target) / 10;
-                rectTransform_2.localPosition += new Vector3(-a, 0f, 0f); // 현 위치, 목표지점, 참조속도, 속도
-                Debug.Log(a);                                                                                   // ref : 참조접근 -> 실시간으로 바뀌는 값 적용 가능
+            float a = Vector3.Distance(rectTransform_1.position, target) / 10;
+            rectTransform_1.localPosition += new Vector3(-a, 0f, 0f); // 현 위치, 목표지점, 참조속도, 속도
 
-                yield return null;
-            }
-            yield return new WaitForSeconds(0.5f);
-            magazine_LogWindow.SetActive(false);
-            isShow = false;
-            rectTransform_2.localPosition = new Vector3(1090, -303, 0);
+            yield return null;
         }
+
+        yield return new WaitForSeconds(0.5f);
+        
+        potion_LogWindow.SetActive(false);
+        isShow = false;
+        rectTransform_1.localPosition = new Vector3(1090, -303, 0);
     }
 }
