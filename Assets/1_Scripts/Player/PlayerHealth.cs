71f82753026f772d24e131ca453e42dd4c7726d0
HeoJongHyun <ghkfls126@naver.com>
2020-11-07 20:30:15 +0900

    PlayerHealth 주석 필요없는 코드 날림

--- Assets/1_Scripts/Player/PlayerHealth.cs
+++ Assets/1_Scripts/Player/PlayerHealth.cs
@@ -5,19 +5,13 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public float startingHealth = 100f; // 시작 체력
+    GameInformation GI;
     public bool isDead { get; protected set; } // 사망 상태
-    public event Action onDeath; // 사망시 발동할 이벤트
-
     private Animator playerAnimator; // 애니메이터 컴포넌트
-
     AudioSource audioSource;
     [Header("-Sound")]
     public AudioClip audioDead;
     public AudioClip[] audioBeAttacked;
-
-    GameInformation GI;
-
     void Start()
     {
         GI = GameInformation.Instance;
@@ -27,11 +21,12 @@ public class PlayerHealth : MonoBehaviour
         // 사운드
         audioSource = gameObject.AddComponent<AudioSource>();
     }
-
+    // 데미지를 받음
     public void OnDamage(float attackPoint)
     {
+        // 죽었으면 리턴 아래 실행 안함
         if (isDead) return;
-
+        // GameInformation의 event unityAction에 등록된 UI쪽 메서드 실행
         // HP -= attackPoint;
         GI.UpdateHp(-attackPoint);
 
@@ -42,20 +37,25 @@ public class PlayerHealth : MonoBehaviour
         Debug.Log("플레이어 공격 받음. 남은체력" + GI.HP);
         if (GI.HP <= 0)
         {
+            // HP가 다 떨어지면 메서드 실행
             StartCoroutine(Die());
         }
     }
-
+    // 죽음
     private IEnumerator Die()
     {
+        // 죽음 판단 bool
         isDead = true;
+        // GameManager에 게임오버 알림
         GameManager.Instance.isGameOver = true;
+        // 애니메이션
         playerAnimator.SetTrigger("Die");
 
         // 사운드
         audioSource.PlayOneShot(audioDead);
-
+        // 애니메이션 기다림
         yield return new WaitForSeconds(3f);
+        // 죽고나면 발동 할 메서드
         GameManager.Instance.PlayerDead();
     }
 }
