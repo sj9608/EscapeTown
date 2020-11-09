3c6c9595f834e2e92c9cfdba95c8aa647f577a4c
HeoJongHyun <ghkfls126@naver.com>
2020-11-09 19:37:22 +0900

    UIGameOver 주석

--- Assets/1_Scripts/UI/UIGameOver.cs
+++ Assets/1_Scripts/UI/UIGameOver.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 
 public class UIGameOver : MonoBehaviour
 {
+    // UI는 Action에 등록하여 화면만 보여준다.
+    // 기능적인 것은 GameManager나 여타 다른 곳에서 구현
+    
     // 게임 오버 화면 UI
     public GameObject gameover;                                   // 게임오버 패널 창
     public Button btn_Retry;                                      // 게임오버 다시하기 버튼
@@ -20,18 +23,22 @@ public class UIGameOver : MonoBehaviour
     }
 
     private void OnEnable() {
+        // GameOver시 보여줘야 할 메서드 Action에 메서드 추가
         GameManager.Instance.GameOverAction += Show_GameOver_Pannel;
     }
     private void OnDisable(){
+        // GameOver시 보여줘야 할 메서드 Action에 메서드 제거
+        // null check 필요
         GameManager.Instance.GameOverAction -= Show_GameOver_Pannel;
     }
+    // GameOver시 보여 줄 UI
     public void Show_GameOver_Pannel()
     {
         gameover.SetActive(true);
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
-
+    // 다시하기 버튼
     public void GameOver_BTN_Retry()
     {   // 가장 마지막으로 저장했던 데이터를 불러오기
         gameover.SetActive(false);
@@ -42,9 +49,10 @@ public class UIGameOver : MonoBehaviour
         // Cursor.lockState = CursorLockMode.Locked;
         // int s = SceneManager.GetActiveScene().buildIndex; // 현재 씬의 인덱스 번호를 다시 불러옴
         // SceneManager.LoadScene(s); // 현재 씬 다시 부르기       
+        // 게임매니저의 다시하기 메서드 호출
         GameManager.Instance.GameRetry();
     }
-    
+    // 메인으로 버튼
     public void GameOver_BTN_Main()
     {   
         gameover.SetActive(false);
